042e8651c9ddd4d33ece73107f6a99a54ea1a2a2
Féras Ghandouri <feras.ghandouri@etu.univ-lyon1.fr>
2020-04-13 17:12:30 +0200

    Ajout de nouveaux controles sur l'interface

--- src/gKitCore/gui.cpp
+++ src/gKitCore/gui.cpp
@@ -4,6 +4,9 @@ Gui::Gui()
 {
     cubemap_id = 0;
     translationEnabled = false;
+    downsample_ratio = 1.0;
+    pow_interp = 1.5;
+     
 }
 
 
@@ -13,7 +16,7 @@ void Gui::init(SDL_Window *window, SDL_GLContext context)
     m_context = context;
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO(); (void)io;
-    ImGui::StyleColorsDark();
+    ImGui::StyleColorsClassic();
     ImGui_ImplSDL2_InitForOpenGL(window, context);
     ImGui_ImplOpenGL3_Init("#version 330");
 }
@@ -30,8 +33,9 @@ void Gui::ButtonSetExpr(const char* label, unsigned int id ) {
 
     int pushedColors = 0;
 
-
+    ImGui::SetCursorPosX(ImGui::GetWindowWidth()/4.0);
     if(ImGui::Button(label, ImVec2(200, 25))) {
+       
         isToggled = true;
         set_expr(id);
     }
@@ -48,7 +52,9 @@ void Gui::draw()
     // Start the Dear ImGui frame
     ImGui_ImplOpenGL3_NewFrame();
     ImGui_ImplSDL2_NewFrame(m_window);
+    
     ImGui::NewFrame();
+    ImGui::SetWindowSize(ImVec2(400, 400));
 
     // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
     {
@@ -61,7 +67,7 @@ void Gui::draw()
         ImGui::Text("Set Expressions");
         ImGui::NewLine();
 
-
+        
         ButtonSetExpr("Neutral", 0);
         ButtonSetExpr("Mouth Open", 1);
         ButtonSetExpr("Jaw Left", 2);
@@ -84,16 +90,26 @@ void Gui::draw()
         ImGui::NewLine();
 
         // Sliders
+        ImGui::PushStyleColor(ImGuiCol_SliderGrab, (ImVec4)ImColor::HSV(9/7.0f, 0.6f, 0.6f));
         ImGui::SliderFloat3(" Mesh position", translation, -10.0, 10.0);
         ImGui::NewLine();
         // color picker
+        ImGui::PushStyleColor(ImGuiCol_SliderGrab, (ImVec4)ImColor::HSV(2/7.0f, 0.6f, 0.6f));
         ImGui::ColorEdit3(" Light color", color);
 
         ImGui::NewLine();
         ImGui::NewLine();
-
+        ImGui::PushStyleColor(ImGuiCol_SliderGrab, (ImVec4)ImColor::HSV(3/7.0f, 0.6f, 0.6f));
         if(ImGui::Button("Enable Translation")) translationEnabled = true;
+        ImGui::NewLine();
+        ImGui::PushStyleColor(ImGuiCol_SliderGrab, (ImVec4)ImColor::HSV(4/7.0f, 0.6f, 0.6f));
+        ImGui::SliderFloat("Interpolation power", &pow_interp, 0, 10.0);
+        ImGui::PushStyleColor(ImGuiCol_SliderGrab, (ImVec4)ImColor::HSV(5/7.0f, 0.6f, 0.6f));
+        ImGui::SliderFloat("Downsampling", &downsample_ratio, 1, 10);
 
+        ImGui::PopStyleColor(5);
+
+        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
 
         ImGui::SetCursorPosY(500);
         ImGui::End();
