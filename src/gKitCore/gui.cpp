450413960fc00de1e54c79b368aa13bf183b0126
Féras Ghandouri <feras.ghandouri@etu.univ-lyon1.fr>
2020-04-13 03:41:17 +0200

    Suppr texte

--- src/gKitCore/gui.cpp
+++ src/gKitCore/gui.cpp
@@ -96,7 +96,6 @@ void Gui::draw()
 
 
         ImGui::SetCursorPosY(500);
-        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
         ImGui::End();
     }
 
